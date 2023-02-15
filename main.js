ab3ccf1a91d2db49fc860e4b75b06a9380402af5
Nate West <nayt@Nates-Air.lan>
2023-02-15 12:30:20 -0600

    initial commit

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let board = [];
 let solution = '';
 let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
-const printBoard = () =>  {
+const printBoard = () =>  {git s
   for (let i = 0; i < board.length; i++) {
     console.log(board[i]);
   }
@@ -28,13 +28,47 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-const generateHint = () =>  {
+const generateHint = (guess) =>  {
   // your code here
+  let solutionArray = solution.split('');   // solution.split  ['a','b','c','d']
+  let guessArray = guess.split('');  // guess.split ['a','d','b','c']
+                      // compare the values at each index
+
+  let correctLetterLocations = 0; // Will record how many correct "letter-locations" were guessed.
+  let correctLetters = 0; //How many correct "letters" not in the correct location
+  // Compare the values at each index with a for loop
+  for(let i=0; i < solutionArray.length; i++){
+    if(solutionArray[i] === guessArray[i]) {
+        correctLetterLocations++;
+        solutionArray[i] = null;
+    }
+  }  
+    // check for correct letters in the wrong location
+
+  for(let i=0; i < solutionArray.length; i++){
+      let targetIndex = solutionArray.indexOf(guessArray[i]);
+      if (targetIndex > -1) {
+        correctLetters++;
+        solutionArray[targetIndex] = null; 
+      }
+    } 
+    // console.log('You have ' + correctLetterLocations + 'letters in the right place, and ' + correctLetters + ' correct letters in the wrong place')
+
+    return correctLetterLocations + '-' + correctLetters;
+
+  
 }
 
 const mastermind = (guess) => {
   solution = 'abcd'; // Comment this out to generate a random solution
-  // your code here
+  if(solution.localeCompare(guess)){
+    return 'You guessed it!';
+  }
+  // Define a variable called hint that collects the returned value of a generateHint(guess).
+  //solution = generateSolution(); // uncomment for random solution
+  let hint = generateHint(guess)
+
+  board.push('${correctLetterLocations} in the right place and ${correctLetters} correct letters')
 }
 
 
